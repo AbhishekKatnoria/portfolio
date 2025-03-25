8cc39ecb36e603b6fd33b20cf91d3de92d7af444
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-03-25 22:51:48 +0530

    Implemented slider

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,9 +1,37 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
 const Header = () => {
+  const items = new Array(10).fill(null);
+
+  const [sliderRef] = useKeenSlider({
+    mode: "free-snap",
+    slides: {
+      origin: "center",
+      perView: 10,
+    },
+    loop: true,
+  });
+
   return (
     <div className="p-8">
-      <Image src="/logo.png" alt="logo" width={20} height={20} />
+      <div
+        ref={sliderRef}
+        className="p-7 flex gap-1 border rounded-4xl keen-slider"
+      >
+        {items.map((_, index) => (
+          <div
+            key={index}
+            className="keen-slider__slide p-4 flex justify-center items-center"
+          >
+            <Image src="/loader.svg" alt="logo" width={20} height={20} />
+            <h2 className="text-[22px]">Portfolio</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
