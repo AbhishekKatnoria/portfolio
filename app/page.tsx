54c34142a57b52e2214edee3c71302c06dd5dbd7
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-03-25 20:03:03 +0530

    Import profile component in page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Header from "./components/Header";
 import Profile from "./components/profile";
-import RootLayout from "./layout";
 
 export default function Home() {
   return (
-    <RootLayout>
-      <Header/>
-      <Profile/>
-    </RootLayout>
+    <>
+      <Profile />
+    </>
   );
 }
