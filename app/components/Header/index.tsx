9f53d7acb4798bb278bd96972a6aa4e758cbc91f
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-03-27 00:28:45 +0530

    Added the functionality of auto scroll

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -5,7 +5,14 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
 const Header = () => {
-  const items = new Array(10).fill(null);
+  const items = new Array(15).fill(null);
+
+  const animation = {
+    duration: 14000,
+    easing: (t) => t,
+  };
+
+  const looping = true;
 
   const [sliderRef] = useKeenSlider({
     mode: "free-snap",
@@ -14,13 +21,30 @@ const Header = () => {
       perView: 10,
     },
     loop: true,
+    created(s) {
+      looping && s.moveToIdx(5, true, animation);
+    },
+    updated(s) {
+      looping && s.moveToIdx(s.track.details.abs + 5, true, animation);
+    },
+    animationEnded(s) {
+      looping && s.moveToIdx(s.track.details.abs + 5, true, animation);
+    },
+    breakpoints: {
+      "(max-width: 768px)": {
+        slides: { perView: 2 },
+      },
+      "(max-width: 480px)": {
+        slides: { perView: 1 },
+      },
+    },
   });
 
   return (
     <div className="p-8">
       <div
         ref={sliderRef}
-        className="p-7 flex gap-1 border rounded-4xl keen-slider"
+        className="p-7 flex gap-1 border border-borderColor rounded-[32px] keen-slider"
       >
         {items.map((_, index) => (
           <div
