02f7023ff08866473250232a51adb88e26d0691d
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-04-06 23:02:25 +0530

    Refactor code structure for improved readability and maintainability

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,56 +1,14 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
-
 const Header = () => {
   const items = new Array(15).fill(null);
 
-  const animation = {
-    duration: 14000,
-    easing: (t: any) => t,
-  };
-
-  const looping = true;
-
-  const [sliderRef] = useKeenSlider({
-    mode: "free-snap",
-    slides: {
-      origin: "center",
-      perView: 10,
-    },
-    loop: true,
-    created(s) {
-      looping && s.moveToIdx(5, true, animation);
-    },
-    updated(s) {
-      looping && s.moveToIdx(s.track.details.abs + 5, true, animation);
-    },
-    animationEnded(s) {
-      looping && s.moveToIdx(s.track.details.abs + 5, true, animation);
-    },
-    breakpoints: {
-      "(max-width: 768px)": {
-        slides: { perView: 2 },
-      },
-      "(max-width: 480px)": {
-        slides: { perView: 1 },
-      },
-    },
-  });
-
   return (
     <div className="p-8">
-      <div
-        ref={sliderRef}
-        className="p-6 flex gap-1 border border-borderColor rounded-[32px] keen-slider"
-      >
+      <div className="p-6 flex gap-1 border border-borderColor rounded-[32px] keen-slider">
         {items.map((_, index) => (
-          <div
-            key={index}
-            className="keen-slider__slide flex justify-center items-center"
-          >
+          <div key={index} className="flex justify-center items-center">
             <Image src="/loader.svg" alt="logo" width={20} height={20} />
             <h2 className="text-[22px]">Portfolio</h2>
           </div>
