3db80b47e0573987bf4bf75fb9e33c4d650f9cc7
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-04-07 21:39:11 +0530

    Added the marquee

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,18 +1,25 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
+
 const Header = () => {
   const items = new Array(15).fill(null);
 
   return (
     <div className="p-8">
       <div className="p-6 flex gap-1 border border-borderColor rounded-[32px] keen-slider">
-        {items.map((_, index) => (
-          <div key={index} className="flex justify-center items-center">
-            <Image src="/loader.svg" alt="logo" width={20} height={20} />
-            <h2 className="text-[22px]">Portfolio</h2>
-          </div>
-        ))}
+        <Marquee pauseOnHover={true}>
+          {items.map((_, index) => (
+            <div
+              key={index}
+              className="flex justify-center items-center px-1 gap-1"
+            >
+              <Image src="/loader.svg" alt="logo" width={20} height={20} />
+              <h2 className="text-[22px]">Portfolio</h2>
+            </div>
+          ))}
+        </Marquee>
       </div>
     </div>
   );
